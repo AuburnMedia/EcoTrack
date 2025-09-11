f1731e12a7040f023634d81014b49322bf58c5c1
AuburnMedia <nicholashoughton2@gmail.com>
2025-09-11 13:17:42 +1000

    fix: close #27 by adding decorator @onboarding_required

---
+++ apps/pages/decorators.py
@@ -0,0 +1,15 @@
+from django.shortcuts import redirect
+from functools import wraps
+from .models import UserProfile
+
+def onboarding_required(view_func):
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        try:
+            profile = UserProfile.objects.get(user=request.user)
+            if not profile.onboarding_completed:
+                return redirect('onboarding')
+        except UserProfile.DoesNotExist:
+            return redirect('onboarding')
+        return view_func(request, *args, **kwargs)
+    return _wrapped_view
