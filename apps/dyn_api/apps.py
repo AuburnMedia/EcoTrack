b718f6a94fb4bda8bfccbd38fc0ab56277bac81e
AuburnMedia <nicholashoughton2@gmail.com>
2025-10-08 13:44:25 +1100

    chore!?: remove unneeded files

--- apps/dyn_api/apps.py
+++
@@ -1,9 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-
-from django.apps import AppConfig
-
-
-class DynApiConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.dyn_api"
